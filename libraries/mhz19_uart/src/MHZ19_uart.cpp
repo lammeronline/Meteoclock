03a457c22c561e447ddc80f06e57f16ab697f03f
lammeronline <lammeronline@gmail.com>
2025-06-30 15:57:41 +0300

    *Replace some library to micro.

--- libraries/mhz19_uart/src/MHZ19_uart.cpp
+++ libraries/mhz19_uart/src/MHZ19_uart.cpp
@@ -8,142 +8,156 @@
 #include "MHZ19_uart.h"
 #include "Arduino.h"
 
-
-#define WAIT_READ_TIMES	100
-#define WAIT_READ_DELAY	10
+#define WAIT_READ_TIMES 100
+#define WAIT_READ_DELAY 10
 
 // public
-
-MHZ19_uart::MHZ19_uart(){
+MHZ19_uart::MHZ19_uart()
+{
 }
-MHZ19_uart::MHZ19_uart(int rx, int tx){
-	begin(rx,tx);
+MHZ19_uart::MHZ19_uart(int rx, int tx)
+{
+	begin(rx, tx);
 }
 
-MHZ19_uart::~MHZ19_uart(){
+MHZ19_uart::~MHZ19_uart()
+{
+}
+
+void MHZ19_uart::begin(int rx, int tx)
+{
+	_rx_pin = rx;
+	_tx_pin = tx;
+}
+
+void MHZ19_uart::setAutoCalibration(boolean autocalib)
+{
+	writeCommand(autocalib ? autocalib_on : autocalib_off);
+}
+
+void MHZ19_uart::calibrateZero()
+{
+	writeCommand(zerocalib);
+}
+
+void MHZ19_uart::calibrateSpan(int ppm)
+{
+	if (ppm < 1000)
+		return;
+
+	uint8_t com[MHZ19_uart::REQUEST_CNT];
+	for (int i = 0; i < MHZ19_uart::REQUEST_CNT; i++)
+	{
+		com[i] = spancalib[i];
+	}
+	com[3] = (uint8_t)(ppm / 256);
+	com[4] = (uint8_t)(ppm % 256);
+	writeCommand(com);
+}
+
+int MHZ19_uart::getCO2PPM()
+{
+	readSerialData();
+	return _co2;
+}
+
+int MHZ19_uart::getTemperature()
+{
+	readSerialData();
+	return _co2temp;
 }
 
 #ifdef ARDUINO_ARCH_ESP32
-void MHZ19_uart::begin(int rx, int tx, int s){
-	_rx_pin = rx;
-	_tx_pin = tx;
-	_serialno = s;
-}
-#else
-void MHZ19_uart::begin(int rx, int tx){
-	_rx_pin = rx;
-	_tx_pin = tx;
+void MHZ19_uart::setHardwareSerialNo(int serialNo)
+{
+	_serialNo = serialNo;
 }
 #endif
 
-void MHZ19_uart::setAutoCalibration(boolean autocalib){
-	writeCommand( autocalib ? autocalib_on : autocalib_off );
-}
-
-void MHZ19_uart::calibrateZero() {
-	writeCommand( zerocalib );
-}
-
-void MHZ19_uart::calibrateSpan(int ppm) {
-	if( ppm < 1000 )	return;
-
-	uint8_t com[MHZ19_uart::REQUEST_CNT];
-	for(int i=0; i<MHZ19_uart::REQUEST_CNT; i++) {
-		com[i] = spancalib[i];
-	}
-	com[3] = (uint8_t)(ppm/256);
-	com[4] = (uint8_t)(ppm%256);
-	writeCommand( com );
-}
-
-int MHZ19_uart::getPPM() {
-	return getSerialData(PPM);
-}
-
-int MHZ19_uart::getTemperature() {
-	return getSerialData(TEMPERATURE);
-}
-
-int MHZ19_uart::getStatus() {
-	return getSerialData(STAT);
-}
-
-boolean MHZ19_uart::isWarming(){
-	return (getStatus() <=1);
-}
-
 //protected
-void MHZ19_uart::writeCommand(uint8_t cmd[]) {
-	writeCommand(cmd,NULL);
+void MHZ19_uart::writeCommand(uint8_t cmd[])
+{
+	writeCommand(cmd, NULL);
 }
 
-void MHZ19_uart::writeCommand(uint8_t cmd[], uint8_t* response) {
+void MHZ19_uart::writeCommand(uint8_t cmd[], uint8_t *response)
+{
 #ifdef ARDUINO_ARCH_ESP32
-	HardwareSerial hserial(_serialno);
+	HardwareSerial hserial(_serialNo);
 	hserial.begin(9600, SERIAL_8N1, _rx_pin, _tx_pin);
 #else
 	SoftwareSerial hserial(_rx_pin, _tx_pin);
 	hserial.begin(9600);
 #endif
-    hserial.write(cmd, REQUEST_CNT);
+	hserial.write(cmd, REQUEST_CNT);
 	hserial.write(mhz19_checksum(cmd));
 	hserial.flush();
-	
-	if (response != NULL) {
+
+	if (response != NULL)
+	{
 		int i = 0;
-		while(hserial.available() <= 0) {
-			if( ++i > WAIT_READ_TIMES ) {
+		while (hserial.available() <= 0)
+		{
+			if (++i > WAIT_READ_TIMES)
+			{
 				Serial.println("error: can't get MH-Z19 response.");
 				return;
 			}
 			yield();
 			delay(WAIT_READ_DELAY);
 		}
-		//Serial.println(String("debug:")+String(i));
 		hserial.readBytes(response, MHZ19_uart::RESPONSE_CNT);
 	}
-
 }
 
 //private
-
-int MHZ19_uart::getSerialData(MHZ19_DATA flg) {
+void MHZ19_uart::readSerialData()
+{
 	uint8_t buf[MHZ19_uart::RESPONSE_CNT];
-	for( int i=0; i<MHZ19_uart::RESPONSE_CNT; i++){
-		buf[i]=0x0;
+	for (int i = 0; i < MHZ19_uart::RESPONSE_CNT; i++)
+	{
+		buf[i] = 0x0;
 	}
 
 	writeCommand(getppm, buf);
-	int co2 = 0, co2temp = 0, co2status =  0;
 
 	// parse
-	if (buf[0] == 0xff && buf[1] == 0x86 && mhz19_checksum(buf) == buf[MHZ19_uart::RESPONSE_CNT-1]) {
-		co2 = buf[2] * 256 + buf[3];
-		co2temp = buf[4] - 40;
-		co2status =  buf[5];
-	} else {
-		co2 = co2temp = co2status = -1;
+	if (buf[0] == 0xff && buf[1] == 0x86 && mhz19_checksum(buf) == buf[MHZ19_uart::RESPONSE_CNT - 1])
+	{
+		_co2 = buf[2] * 256 + buf[3];
+		_co2temp = buf[4] - 40;
+		_co2status = buf[5];
 	}
-
-	switch(flg) {
-		case MHZ19_DATA::TEMPERATURE:
-			return co2temp;
-			break;
-		case MHZ19_DATA::STAT:
-			return co2status;
-			break;
-		case MHZ19_DATA::PPM:
-		default:
-			return co2;
-			break;
+	else
+	{
+		_co2 = _co2temp = _co2status = -1;
 	}
-}	
+}
 
-uint8_t MHZ19_uart::mhz19_checksum( uint8_t com[] ) {
+uint8_t MHZ19_uart::mhz19_checksum(uint8_t com[])
+{
 	uint8_t sum = 0x00;
-	for ( int i = 1; i < MHZ19_uart::REQUEST_CNT; i++) {
+	for (int i = 1; i < MHZ19_uart::REQUEST_CNT; i++)
+	{
 		sum += com[i];
 	}
 	sum = 0xff - sum + 0x01;
 	return sum;
+}
+
+//deprecated
+int MHZ19_uart::getPPM()
+{
+	return getCO2PPM();
+}
+//deprecated
+int MHZ19_uart::getStatus()
+{
+	return 0;
+}
+//deprecated
+boolean MHZ19_uart::isWarming()
+{
+	delay(10 * 1000);
+	return true;
 }
--- libraries/mhz19_uart/src/MHZ19_uart.h
+++ libraries/mhz19_uart/src/MHZ19_uart.h
@@ -10,14 +10,10 @@
 
 #include "Arduino.h"
 #ifdef ARDUINO_ARCH_ESP32
-	#include "HardwareSerial.h"
+#include "HardwareSerial.h"
 #else
-	#include "SoftwareSerial.h"
+#include "SoftwareSerial.h"
 #endif
-
-enum MHZ19_DATA { 
-	PPM, TEMPERATURE, STAT 
-};
 
 class MHZ19_uart
 {
@@ -26,19 +22,22 @@ public:
 	MHZ19_uart(int rx, int tx);
 	virtual ~MHZ19_uart();
 
-#ifdef ARDUINO_ARCH_ESP32
-	void begin(int rx=-1, int tx=-1, int s=1);
-#else
-	void begin(int rx=-1, int tx=-1);
-#endif
+	void begin(int rx = -1, int tx = -1);
 	void setAutoCalibration(boolean autocalib);
 	void calibrateZero();
 	void calibrateSpan(int ppm);
-	
-	int getPPM();
+
+	int getCO2PPM();
 	int getTemperature();
+#ifdef ARDUINO_ARCH_ESP32
+	void setHardwareSerialNo(int serialNo);
+#endif
+
+	//deprecated
+	int getPPM();
+	//deprecated
 	int getStatus();
-	
+	//deprecated
 	boolean isWarming();
 
 protected:
@@ -46,23 +45,27 @@ protected:
 	void writeCommand(uint8_t com[], uint8_t response[]);
 
 private:
-	uint8_t mhz19_checksum( uint8_t com[] );
-	int getSerialData(MHZ19_DATA flg);
+	uint8_t mhz19_checksum(uint8_t com[]);
+	void readSerialData();
 
 	static const int REQUEST_CNT = 8;
 	static const int RESPONSE_CNT = 9;
 
+	int _co2 = 0;
+	int _co2temp = 0;
+	int _co2status = 0;
+
 	// serial command
-	uint8_t getppm[REQUEST_CNT]			= {0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00};
-	uint8_t zerocalib[REQUEST_CNT]	 	= {0xff, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00};
-	uint8_t spancalib[REQUEST_CNT]	 	= {0xff, 0x01, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00};
-	uint8_t autocalib_on[REQUEST_CNT] 	= {0xff, 0x01, 0x79, 0xA0, 0x00, 0x00, 0x00, 0x00};
-	uint8_t autocalib_off[REQUEST_CNT]	= {0xff, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00};
-#ifdef ARDUINO_ARCH_ESP32
-	int _serialno = 1;
-#endif
+	uint8_t getppm[REQUEST_CNT] = {0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00};
+	uint8_t zerocalib[REQUEST_CNT] = {0xff, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00};
+	uint8_t spancalib[REQUEST_CNT] = {0xff, 0x01, 0x88, 0x00, 0x00, 0x00, 0x00, 0x00};
+	uint8_t autocalib_on[REQUEST_CNT] = {0xff, 0x01, 0x79, 0xA0, 0x00, 0x00, 0x00, 0x00};
+	uint8_t autocalib_off[REQUEST_CNT] = {0xff, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00};
 	int _rx_pin = -1;
 	int _tx_pin = -1;
+#ifdef ARDUINO_ARCH_ESP32
+	int _serialNo = 2;
+#endif
 };
 
 #endif
